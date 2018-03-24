eafd42a30597a1a2cc16d4ec6ec1c9544d8219d1
Adrianacmy <adrianamullaney@gmail.com>
2018-03-23 17:41:11 -0700

    code js to show user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,31 @@
-$().ready(function(){
+var taxCaculator = function(state){
+	var tax = 0.08;
+	if (state === "oregon"){
+		tax = 0;
+	}
+	return tax;
+}
 
+$().ready(function(){
+	$("#request-form").submit(function (e) {
+		e.preventDefault();
+		var location = $("input:radio[name=location]:checked").val();
+		var cat = $("input:radio[name=cat]:checked").val();
+		var brands = $("#brands").val();
+		var message = $("#message").val();
+		var firstname = $("#firstname").val();
+		var lasttname = $("#lastname").val();
+		var email = $("#email").val();
+		var phone = $("#phone").val();
+
+		$("#request-form").hide();
+		$("#output").show();
+
+		var taxRate = taxCaculator(location);
+		var list = [firstname, lasttname, email, phone, location, cat, brands, message, taxRate];
+
+		list.forEach(function (item) {
+			$("#output").append("<p class='lead'>" + item + "</p>");
+		});
+	});
 });
